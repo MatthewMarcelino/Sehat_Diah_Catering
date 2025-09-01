b52c1c83b8b15d1a9c93587826c5ecc24d6bf254
MatthewMarcelino <matthewseko221@gmail.com>
2025-09-01 21:46:16 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ['./index.html'], // sesuaikan dengan struktur proyekmu
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'], // <== tambahkan ini
+      },
+    },
+  },
+  plugins: [],
+}
